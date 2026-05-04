6292f75ca5625e0e5873eea2d7c856a590d2ce18
Duong <trieuduongdc@gmail.com>
2026-05-04 17:35:35 +0200

    feat: add Router component to manage application routing and display MeasurementsDashboardPage

---
+++ my-react-app/src/app/router.tsx
@@ -0,0 +1,7 @@
+import { MeasurementsDashboardPage } from "../features/measurements/pages/MeasurementsDashboardPage";
+
+// This file is intentionally simple for now.
+// Later, you can replace this with react-router-dom if you add multiple pages.
+export function Router() {
+  return <MeasurementsDashboardPage />;
+}
